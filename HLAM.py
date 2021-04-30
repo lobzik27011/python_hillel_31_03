d87a36417898cabab069b66a111e687e56888f18
lobzik27011 <kovalinskiy27011@gmail.com>
2021-04-30 14:42:38 +0300

    Home Work 6

---
+++ HLAM.py
@@ -0,0 +1,27 @@
+# 7. Даны две строки. Создать список в который поместить те символы, которые есть в обеих строках,
+# но в каждой только по одному разу.
+
+# my_str_1 = 'сихафазатрон'
+# my_str_2 = 'ликвидность'
+# my_result_1 = []
+# my_result_2 = []
+# for i in my_str_1:
+#     if my_str_1.count(i) == 1:
+#         my_result_1.append(i)
+# for i in my_str_2:
+#     if my_str_2.count(i) == 1:
+#         my_result_2.append(i)
+# my_result = set(my_result_1).intersection(set(my_result_2))
+# print(my_result)
+
+my_str_1 = 'сихафазатрон'
+my_str_2 = 'ликвидность'
+my_result = []
+
+for i in my_str_1:
+    if my_str_1.count(i) == 1 and my_str_2.count(i) == 1:
+        my_result.append(i)
+print(my_result)
+
+
+##################################################################################################
--- HW6.py
+++ HW6.py
@@ -58,11 +58,6 @@ for i in my_str:
         my_result.append(i)
 print(my_result)
 
-
-my_str = 'Я люблю кушать бургеры =)))'
-my_result = list(set(my_str))
-print(my_result)
-
 ##################################################################################################
 # 6. Даны две строки. Создать список в который поместить те символы,
 # которые есть в обеих строках хотя бы раз.
@@ -77,19 +72,6 @@ print(list(my_result))
 # 7. Даны две строки. Создать список в который поместить те символы, которые есть в обеих строках,
 # но в каждой только по одному разу.
 
-# my_str_1 = 'сихафазатрон'
-# my_str_2 = 'ликвидность'
-# my_result_1 = []
-# my_result_2 = []
-# for i in my_str_1:
-#     if my_str_1.count(i) == 1:
-#         my_result_1.append(i)
-# for i in my_str_2:
-#     if my_str_2.count(i) == 1:
-#         my_result_2.append(i)
-# my_result = set(my_result_1).intersection(set(my_result_2))
-# print(my_result)
-
 my_str_1 = 'сихафазатрон'
 my_str_2 = 'ликвидность'
 my_result = []
@@ -98,7 +80,6 @@ for i in my_str_1:
     if my_str_1.count(i) == 1 and my_str_2.count(i) == 1:
         my_result.append(i)
 print(my_result)
-
 
 ##################################################################################################
 # 8. Описать с помощью словаря следующую структуру для конкретного человека (можно придумать):
